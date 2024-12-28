fe775d799bb204aab8f33d80ed801db87e8596a6
Ravi Kalmodiya <ravikalmodiya01@gmail.com>
2024-12-28 10:34:19 +0530

    new

---
+++ q11.js
@@ -0,0 +1,7 @@
+let arr = [33,45,53,34,29,42,31]
+
+let four = arr[3]
+
+arr[3] =  four+2
+
+console.log(arr)
---
+++ q12.js
@@ -0,0 +1,7 @@
+let arr = ["walk up" , "read book" , "take bath" , "game" , "collage"]
+
+arr.unshift("Exrsice")
+
+arr.pop()
+
+console.log(arr)
